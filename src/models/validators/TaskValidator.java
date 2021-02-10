2ab1cea820c3ec47b61ebd8a06429d4ab79e850b
yasushi.iwai <y.iwai.japan@gmail.com>
2021-02-11 03:29:22 +0900

    Initial commit

---
+++ src/models/validators/TaskValidator.java
@@ -0,0 +1,40 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Task;
+
+public class TaskValidator {
+    public static List<String> validate(Task t) {
+        List<String> errors = new ArrayList<String>();
+
+        String title_error = validateTitle(t.getTitle());
+        if(!title_error.equals("")) {
+            errors.add(title_error);
+        }
+
+        String content_error = validateContent(t.getContent());
+        if(!content_error.equals("")) {
+            errors.add(content_error);
+        }
+        return errors;
+    }
+
+    private static String validateTitle(String title) {
+        if(title == null || title.equals("")) {
+            return "タイトルを入力してください。";
+        }
+
+        return "";
+    }
+
+    private static String validateContent(String content) {
+        if(content == null|| content.equals("")) {
+            return "タスクを入力してください";
+        }
+
+        return "";
+    }
+
+}
